1aa4fd9935b7226ca3af409a4756015e37571b9a
gehlisboa <gehmoraeslisboa@gmail.com>
2026-03-18 18:22:05 -0300

    parametrizacao

--- app/Http/Controllers/Contato.php
+++ app/Http/Controllers/Contato.php
@@ -7,18 +7,32 @@ use Illuminate\Http\Request;
 class Contato extends Controller
 {
     function contato(string $nome){
-        echo "O nome do contato é: $nome";
+        // echo "O nome do contato é: $nome";
+        $contato = [
+            "nome" => $nome
+        ];
+        return view('contato', $contato); 
     }
+
+    function contatoNomeCompleto(string $nome, string $sobrenome){
+        echo "O nome do completo é: $nome $sobrenome";
 }
 
-function contato(string $nome, string $sobrenome){
-    echo "O nome do completo é: $nome $sobrenome";
+
+    function assunto(string $nome, string $sobrenome, string $assunto){
+        echo "<h1>Contato: $nome $sobrenome <h1/>";
+        echo $assunto;
 }
 
-
-function assunto(string $nome, string $sobrenome, string $assunto){
-    echo "<h1>Contato: $nome $sobrenome <h1/>";
-    echo $assunto;
+    function mensagem(string $nome, string $sobrenome, string $assunto, string $email, string $telefone = 'Telefone não informado'){
+        echo "<h1>Contato: $nome $sobrenome <h1/>";
+        echo "<h1>$assunto<h1/>";
+        echo "<h1>$email<h1/>";
+        echo $telefone;
 }
+};
+
+
+
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,26 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal'])->name('pagina-inicial');
+
+Route::prefix('publico')->group(function(){
+
 Route::get('/contato/{nome}', [App\Http\Controllers\Contato::class, 'contato']);
 
-Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Contato::class, 'contatoNomeCompleto']);Route::get('/contato/{nome}', [App\Http\Controllers\Contato::class, 'contato']);
+Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Contato::class, 'contatoNomeCompleto']);
 
-Route::get('/contato/{nome}/{sobrenome}/{assunto}', [App\Http\Controllers\Contato::class, 'assunto']);Route::get('/contato/{nome}/{assunto}', [App\Http\Controllers\Contato::class, 'contato']);
+Route::get('/contato/{nome}/{sobrenome}/{assunto}', [App\Http\Controllers\Contato::class, 'assunto']);
+
+Route::get('/contato/{nome}/{sobrenome}/{assunto}/{email}/{telefone?}', [App\Http\Controllers\Contato::class, 'mensagem']);
+});
 
 
 
+
+Route::fallback(function(){
+    echo"A rota acessada não existe";
+    echo"<a href='" . route('pagina-inicial') . "'>Voltar</a>";
+});
 
 
 
